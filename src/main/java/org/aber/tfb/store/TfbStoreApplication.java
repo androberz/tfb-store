e1a82282f44c9bd410bc364b366d6763cbb579d9
Andrey <aberezin@natera.com>
2018-04-27 15:56:46 +0300

    Added Application class.

---
+++ src/main/java/org/aber/tfb/store/TfbStoreApplication.java
@@ -0,0 +1,12 @@
+package org.aber.tfb.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfbStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfbStoreApplication.class, args);
+    }
+}
